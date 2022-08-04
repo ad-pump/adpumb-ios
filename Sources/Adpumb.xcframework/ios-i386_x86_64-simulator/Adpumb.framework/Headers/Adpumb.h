40030fa180437ff0d69d1a19e5cbf56e95120f17
Arjun <arjunrajmp@gmail.com>
2022-08-04 19:55:13 +0530

    Revert "1.2.7"
    This reverts commit 4639b6f24e1e3d0bc4b4fda86a25787a983a8fec.

---
+++ Sources/Adpumb.xcframework/ios-i386_x86_64-simulator/Adpumb.framework/Headers/Adpumb.h
@@ -0,0 +1,18 @@
+//
+//  Adpumb.h
+//  Adpumb
+//
+//  Created by admin on 19/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Adpumb.
+FOUNDATION_EXPORT double AdpumbVersionNumber;
+
+//! Project version string for Adpumb.
+FOUNDATION_EXPORT const unsigned char AdpumbVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Adpumb/PublicHeader.h>
+
+
